f6dddc171a152cc34ed00fea3302b548f1767634
willy <willybalt.2017@gmail.com>
2026-01-31 18:46:11 -0400

    cards

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // quitar cuando no se use github pages
+  base: '/',
+  // base: '/landing-react-cursos/', // <--- importante para GH Pages
 })
